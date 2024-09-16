8121b6644c2c9a2220f42dffeeb7ce40d64104aa
yvant <yvantejondon@gmail.com>
2024-09-16 16:34:24 +0200

    Base pour la page de connexion

---
+++ all4sport/lib/screens/connexion.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ConnectScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Scaffold(
+      appBar: AppBar(
+        title: const Text('Connexion'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: 
+    Column(
+       mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: 
+     InputDecoration(labelText: 'login'),
+            ),
+            const TextField(
+             obscureText: true,
+             decoration: InputDecoration(labelText: 'Mot de passe'),  
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              child: const Text('Se connecter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+} 
