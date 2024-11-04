1583d2032ffdde81c37b2ec2bfbeb7ad4aa240ce
nathanaelTR <nathanael.trt@outlook.fr>
2024-11-04 13:41:54 +0100

    1.5.9 - Mise en place de la Géolocalisation des Entrepôt

--- all4sport/lib/screens/arrivage.dart
+++ all4sport/lib/screens/arrivage.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class ArrivageScreen extends StatefulWidget {
   const ArrivageScreen({super.key});
+
   @override
   _ArrivageScreenState createState() => _ArrivageScreenState();
 }
@@ -13,11 +14,110 @@ class _ArrivageScreenState extends State<ArrivageScreen> {
   String _productReference = '';
   String _warehouse = '';
   int _quantity = 0;
-  int index = 0;
-  final List<String> customizations = [
-    'Customization1',
-    'Customization2'
-  ]; // Example list
+  bool isLoading = false;
+  bool isWarehouseDetected = false;
+  bool isLocationFailed = false;
+
+  final TextEditingController _warehouseController = TextEditingController();
+
+  final List<Map<String, dynamic>> warehouses = [
+    {'name': 'Valenciennes', 'latitude': 50.35, 'longitude': 3.52},
+    {'name': 'Lille', 'latitude': 50.63, 'longitude': 3.05},
+    {'name': 'Nice', 'latitude': 43.70, 'longitude': 7.26}
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      _showLocationError();
+      return;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        _showLocationError();
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      _showLocationError();
+      return;
+    }
+
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+    );
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings);
+      _checkNearestWarehouse(position.latitude, position.longitude);
+    } catch (e) {
+      print("Erreur lors de la récupération de la localisation: $e");
+      _showLocationError();
+    }
+  }
+
+  void _checkNearestWarehouse(double userLatitude, double userLongitude) {
+    const double tolerance = 0.05;
+    String detectedWarehouse =
+        'Saisir un Entrepôt (Aucun Entrepot n\'est référencer dans cette ville)';
+
+    for (var warehouse in warehouses) {
+      double latDiff = (userLatitude - warehouse['latitude']).abs();
+      double lonDiff = (userLongitude - warehouse['longitude']).abs();
+      print(latDiff < tolerance && lonDiff < tolerance);
+      if (latDiff < tolerance && lonDiff < tolerance) {
+        detectedWarehouse = warehouse['name'];
+        isWarehouseDetected = true;
+        break;
+      }
+    }
+
+    setState(() {
+      _warehouse = detectedWarehouse;
+      _warehouseController.text =
+          detectedWarehouse; // Mettez à jour le contrôleur
+      isLoading = false;
+      isLocationFailed = !isWarehouseDetected;
+    });
+
+    if (!isWarehouseDetected) {
+      _showLocationError();
+    }
+  }
+
+  void _showLocationError() {
+    setState(() {
+      isLoading = false;
+      isLocationFailed = true;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text(
+            'Géolocalisation échouée ou lieu non détecté. Veuillez entrer manuellement l\'entrepôt.'),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _warehouseController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,21 +140,23 @@ class _ArrivageScreenState extends State<ArrivageScreen> {
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Veuillez entrer la référence du produit';
-                  } else {}
+                  }
                   return null;
                 },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Entrepôt'),
+                controller: _warehouseController, // Utilisez le contrôleur
                 onSaved: (value) {
                   _warehouse = value!;
                 },
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Veuillez entrer l\'entrepôt';
-                  } else {}
+                  }
                   return null;
                 },
+                readOnly: !isLocationFailed && isWarehouseDetected,
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Quantité'),
@@ -66,9 +168,10 @@ class _ArrivageScreenState extends State<ArrivageScreen> {
                   if (value == null || value.isEmpty) {
                     return 'Veuillez entrer la quantité';
                   }
-                  if (int.tryParse(value) == null) {
-                    return 'Veuillez entrer un nombre valide';
-                  } else {}
+                  final quantity = int.tryParse(value);
+                  if (quantity == null || quantity <= 0) {
+                    return 'Veuillez entrer un nombre valide et positif';
+                  }
                   return null;
                 },
               ),
@@ -82,24 +185,15 @@ class _ArrivageScreenState extends State<ArrivageScreen> {
                       'entrepot': _warehouse,
                       'quantité': _quantity,
                     };
-
-                    // Affichage du JSON dans la console
                     print(jsonEncode(productData));
                   }
                 },
                 child: const Text('Ajouter le nouveau produit'),
               ),
+              if (isLoading) const CircularProgressIndicator(),
             ],
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            index = (index + 1) % customizations.length;
-          });
-        },
-        child: const Icon(Icons.qr_code_2),
       ),
     );
   }
